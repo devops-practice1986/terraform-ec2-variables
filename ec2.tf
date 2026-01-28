c7faa637923b1a97bb41c5a1962cd1141fe6c093
Yellow1986 <cnusrinivasu@gmail.com>
2026-01-28 22:15:04 +0530

    ec2-variables

---
+++ ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "ec2_instance" {
+  
+  ami           = var.ami_id
+  instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.sg.id]
+
+
+  tags = {
+    Name = var.sg_tag_name
+  }
+}
+
+resource "aws_security_group" "sg" {
+  name        = var.security_group_name
+  description = var.sg_description
+
+  ingress {
+    from_port   = var.ssh_port
+    to_port     = var.ssh_port
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.allowed_cidr]
+  }
+  tags = {
+    Name = var.sg_tag_name
+  }
+}   
---
+++ variables.tf
@@ -0,0 +1,54 @@
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+  default = "ami-0220d79f3f480ecf5"
+}
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ssh_port" {
+  description = "The SSH port for the security group"
+  type        = number
+  default     = 22
+}
+variable "allowed_cidr" {
+  description = "The CIDR block allowed to access the SSH port"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+# for tags
+variable "instance_name" {
+  description = "The name tag for the EC2 instance"
+  type        = string
+  default     = "expens"
+}
+variable "security_group_name" {
+  description = "The name of the security group"
+  type        = string
+  default     = "SSH_SG"
+}
+variable "sg_description" {
+  description = "The description of the security group"
+  type        = string
+  default     = "security group for SSH access"
+}
+
+variable "sg_tag_name" {
+  description = "The tag name for the security group"
+  type        = string
+  default     = "Allow_SSH_SG"
+}
+
+
+
+
